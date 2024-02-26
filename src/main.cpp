6bf29335ac16b06c6d122b13b01bfd7478bd4040
Aitor Sanchez <lil.aitor55@gmail.com>
2024-02-26 17:46:56 +0100

    Sesion 4. Todos ejercicios hechos.

--- src/main.cpp
+++ src/main.cpp
@@ -13,32 +13,32 @@ void funKey             (GLFWwindow* window, int key, int scancode, int action, 
 void funScroll          (GLFWwindow* window, double xoffset, double yoffset);
 
 // Shaders
-Shaders shaders;
+   Shaders shaders;
 
 // Modelos
-Model cube;
-Model triangle;
+   Model cube;
+   Model triangle;
 
 // Viewport
-int w = 500;
-int h = 500;
+   int w = 500;
+   int h = 500;
 
 // Control de la escena
-bool  ortho = false;
-float theta =   0.0;
-float desZ  =   0.0;
-float fovy  =  90.0;
+   bool  ortho = false;
+   float theta =   0.0;
+   float desZ  =   0.0;
+   float fovy  =  90.0;
 
 int main() {
 
-    // Inicializamos GLFW
+ // Inicializamos GLFW
     if(!glfwInit()) return -1;
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 
-    // Creamos la ventana
+ // Creamos la ventana
     GLFWwindow* window;
     window = glfwCreateWindow(w, h, "Sesion 4", NULL, NULL);
     if(!window) {
@@ -48,7 +48,7 @@ int main() {
     glfwMakeContextCurrent(window);
     glfwSwapInterval(1);
 
-    // Inicializamos GLEW
+ // Inicializamos GLEW
     glewExperimental = GL_TRUE;
     GLenum err = glewInit();
     if(GLEW_OK != err) {
@@ -59,12 +59,12 @@ int main() {
     const GLubyte *oglVersion = glGetString(GL_VERSION);
     std::cout <<"This system supports OpenGL Version: " << oglVersion << std::endl;
 
-    // Configuramos los CallBacks
+ // Configuramos los CallBacks
     glfwSetFramebufferSizeCallback(window, funFramebufferSize);
     glfwSetKeyCallback            (window, funKey);
     glfwSetScrollCallback         (window, funScroll);
 
-    // Entramos en el bucle de renderizado
+ // Entramos en el bucle de renderizado
     configScene();
     while(!glfwWindowShouldClose(window)) {
         renderScene();
@@ -79,14 +79,14 @@ int main() {
 
 void configScene() {
 
-    // Test de profundidad
+ // Test de profundidad
     glEnable(GL_DEPTH_TEST);
     glPolygonOffset(1.0,1.0);
 
-    // Shaders
+ // Shaders
     shaders.initShaders("resources/shaders/vshader.glsl","resources/shaders/fshader.glsl");
 
-    // Modelos
+ // Modelos
     cube.initModel("resources/models/cube.obj");
     triangle.initModel("resources/models/triangle.obj");
 
@@ -94,14 +94,14 @@ void configScene() {
 
 void renderScene() {
 
-    // Borramos el buffer de color
+ // Borramos el buffer de color
     glClearColor(0.0, 0.0, 0.0, 0.0);
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-    // Indicamos los shaders a utilizar
+ // Indicamos los shaders a utilizar
     shaders.useShaders();
 
-    // Matriz P
+ // Matriz P
     float nplane =  0.1;
     float fplane = 25.0;
     float aspect = (float)w/(float)h;
@@ -109,13 +109,13 @@ void renderScene() {
     if(ortho) P = glm::ortho(-4.0f, 4.0f , -4.0f, 4.0f, nplane, fplane);
     else      P = glm::perspective(glm::radians(fovy), aspect, nplane, fplane);
 
-    // Matriz V
+ // Matriz V
     glm::vec3 eye   ( 0.0,  0.0, desZ);
     glm::vec3 center( 0.0, -0.5, -5.0);
     glm::vec3 up    ( 0.0,  1.0,  0.0);
     glm::mat4 V = glm::lookAt(eye, center, up);
 
-    // Dibujamos la escena
+ // Dibujamos la escena
     glm::mat4 T = glm::translate(I, glm::vec3(0.0, -2.0, -5.0));
     glm::mat4 M = T;
     drawObject(cube, glm::vec3(1.0, 0.0, 0.0), P, V, M);
@@ -133,8 +133,8 @@ void drawObject(Model &model, glm::vec3 color, glm::mat4 P, glm::mat4 V, glm::ma
     shaders.setMat4("uPVM",P*V*M);
 
     glEnable(GL_POLYGON_OFFSET_FILL);
-    shaders.setVec3("uColor",color);
-    model.renderModel(GL_FILL);
+        shaders.setVec3("uColor",color);
+        model.renderModel(GL_FILL);
     glDisable(GL_POLYGON_OFFSET_FILL);
 
     shaders.setVec3("uColor",glm::vec3(1.0, 1.0, 1.0));
@@ -144,10 +144,10 @@ void drawObject(Model &model, glm::vec3 color, glm::mat4 P, glm::mat4 V, glm::ma
 
 void funFramebufferSize(GLFWwindow* window, int width, int height) {
 
-    // Configuracion del Viewport
+ // Configuracion del Viewport
     glViewport(0, 0, width, height);
 
-    // Actualizacion de w y h
+ // Actualizacion de w y h
     w = width;
     h = height;
 
@@ -160,16 +160,16 @@ void funKey(GLFWwindow* window, int key, int scancode, int action, int mods) {
         case GLFW_KEY_Y: if(action==GLFW_REPEAT) theta += 5.0; break;
         case GLFW_KEY_Z:
             if(action==GLFW_PRESS) {
-                if(mods == GLFW_MOD_SHIFT) desZ -= desZ > -2.95 ? 0.1 : 0.0;
-                else                       desZ += desZ <  2.95 ? 0.1 : 0.0;
+               if(mods == GLFW_MOD_SHIFT) desZ -= desZ > -2.95 ? 0.1 : 0.0;
+               else                       desZ += desZ <  2.95 ? 0.1 : 0.0;
             }
             break;
         case GLFW_KEY_R:
             if(action==GLFW_PRESS) {
-                ortho = false;
-                theta =   0.0;
-                desZ  =   0.0;
-                fovy  =  90.0;
+               ortho = false;
+               theta =   0.0;
+               desZ  =   0.0;
+               fovy  =  90.0;
             }
             break;
     }
