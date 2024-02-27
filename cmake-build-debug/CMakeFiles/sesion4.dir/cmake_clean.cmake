9f81e13e4c15de04909e42ba3f62bfea15ea8341
Aitor Sanchez <lil.aitor55@gmail.com>
2024-02-27 22:10:09 +0100

    Inicio sesión 6

---
+++ cmake-build-debug/CMakeFiles/sesion4.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "C:/Users/aitor/Documents/IG/sesion6/binary/libsesion4.dll.a"
+  "C:/Users/aitor/Documents/IG/sesion6/binary/sesion4.exe"
+  "C:/Users/aitor/Documents/IG/sesion6/binary/sesion4.exe.manifest"
+  "C:/Users/aitor/Documents/IG/sesion6/binary/sesion4.pdb"
+  "CMakeFiles/sesion4.dir/src/Model.cpp.obj"
+  "CMakeFiles/sesion4.dir/src/Model.cpp.obj.d"
+  "CMakeFiles/sesion4.dir/src/Shaders.cpp.obj"
+  "CMakeFiles/sesion4.dir/src/Shaders.cpp.obj.d"
+  "CMakeFiles/sesion4.dir/src/main.cpp.obj"
+  "CMakeFiles/sesion4.dir/src/main.cpp.obj.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/sesion4.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
