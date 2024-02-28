5f18c1ff4b7eed764612d49683b7f7f1aee6219e
Aitor Sanchez <lil.aitor55@gmail.com>
2024-02-28 18:07:30 +0100

    Paso 5.

--- binary/resources/shaders/fshader.glsl
+++ binary/resources/shaders/fshader.glsl
@@ -1,10 +1,68 @@
 #version 330 core
 
-in  vec4 vColor;
+struct Light {
+    vec3  position;
+    vec3  direction;
+    vec3  ambient;
+    vec3  diffuse;
+    vec3  specular;
+    float innerCutOff;
+    float outerCutOff;
+    float c0;
+    float c1;
+    float c2;
+};
+
+struct Material {
+    vec4  ambient;
+    vec4  diffuse;
+    vec4  specular;
+    vec4  emissive;
+    float shininess;
+};
+
+#define NLD 1
+
+uniform Light    ulightG;
+uniform Light    ulightD[NLD];
+uniform Material umaterial;
+uniform vec3     ueye;
+
+in  vec3 vnor;
+in  vec3 vpos;
+
 out vec4 outColor;
+
+vec3 funDirectional(Light light, Material material, vec3 N, vec3 V);
 
 void main() {
 
-    outColor = vColor;
+    vec3 N = normalize(vnor);
+    vec3 V = normalize(ueye - vpos);
+
+    vec3 color = umaterial.emissive.rgb + ulightG.ambient * umaterial.ambient.rgb;
+    for(int i=0; i<NLD; i++) color += funDirectional(ulightD[i],umaterial,N,V);
+
+    outColor = vec4(color, umaterial.diffuse.a);
+
+}
+
+vec3 funDirectional(Light light, Material material, vec3 N, vec3 V) {
+
+    vec3  L = normalize(-light.direction);
+    vec3  R = normalize(reflect(-L,N));
+
+    float dotLN = dot(L,N);
+    float dotRV = 0.0;
+    if(dotLN<0.0) dotLN = 0.0;
+    else          dotRV = max(dot(R,V), 0.0);
+    
+    vec3  ambient  = light.ambient  * material.ambient.rgb;
+    vec3  diffuse  = light.diffuse  * material.diffuse.rgb  * dotLN;
+    vec3  specular = light.specular * material.specular.rgb * pow(dotRV,material.shininess);
+
+    vec3 color = ambient + diffuse + specular;
+
+    return color;
 
 }
--- binary/resources/shaders/vshader.glsl
+++ binary/resources/shaders/vshader.glsl
@@ -1,42 +1,14 @@
 #version 330 core
 
-struct Light {
-    vec3  position;
-    vec3  direction;
-    vec3  ambient;
-    vec3  diffuse;
-    vec3  specular;
-    float innerCutOff;
-    float outerCutOff;
-    float c0;
-    float c1;
-    float c2;
-};
-
-struct Material {
-    vec4  ambient;
-    vec4  diffuse;
-    vec4  specular;
-    vec4  emissive;
-    float shininess;
-};
-
-#define NLD 1
-
 layout (location = 0) in vec3 inPosition;
 layout (location = 1) in vec3 inNormal;
 
-uniform mat4     uN;
-uniform mat4     uM;
-uniform mat4     uPVM;
-uniform vec3     ueye;
-uniform Light    ulightG;
-uniform Light    ulightD[NLD];
-uniform Material umaterial;
+uniform mat4 uN;
+uniform mat4 uM;
+uniform mat4 uPVM;
 
-out vec4 vColor;
-
-vec3 funDirectional(Light light, Material material, vec3 N, vec3 V);
+out vec3 vnor;
+out vec3 vpos;
 
 void main() {
 
@@ -45,32 +17,7 @@ void main() {
 
     gl_Position = uPVM * inPositionH;
 
-    vec3 N = normalize((uN*inNormalH).xyz);
-    vec3 V = normalize(ueye - (uM*inPositionH).xyz);
-
-    vec3 color = umaterial.emissive.rgb + ulightG.ambient * umaterial.ambient.rgb;
-    for(int i=0; i<NLD; i++) color += funDirectional(ulightD[i],umaterial,N,V);
-
-    vColor = vec4(color, umaterial.diffuse.a);
-
-}
-
-vec3 funDirectional(Light light, Material material, vec3 N, vec3 V) {
-
-    vec3  L = normalize(-light.direction);
-    vec3  R = normalize(reflect(-L,N));
-
-    float dotLN = dot(L,N);
-    float dotRV = 0.0;
-    if(dotLN<0.0) dotLN = 0.0;
-    else          dotRV = max(dot(R,V), 0.0);
-    
-    vec3  ambient  = light.ambient  * material.ambient.rgb;
-    vec3  diffuse  = light.diffuse  * material.diffuse.rgb  * dotLN;
-    vec3  specular = light.specular * material.specular.rgb * pow(dotRV,material.shininess);
-
-    vec3 color = ambient + diffuse + specular;
-
-    return color;
+    vnor = (uN*inNormalH).xyz;
+    vpos = (uM*inPositionH).xyz;
 
 }
